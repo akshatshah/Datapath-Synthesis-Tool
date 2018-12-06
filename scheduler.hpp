e5d9817a7afbb09e6488c04015da642d3a5713f9
Nicholas <nickowens501@yahoo.com>
2018-12-06 05:31:13 -0500

    Added constraint checks for each operation

--- scheduler.hpp
+++ scheduler.hpp
@@ -1,19 +1,42 @@
-void  LIST_L (vector<struct operation> G, int a){
+void  LIST_L (vector<struct operation> G, int a, int b, int c, int d){
     int size = G.size();
     // l is timestep
     int l = 1;
     do {
         for(int op = 0; op < G.size() - 1; op++)
         {
-            int operationNum = 1;
-            if(G[op].bool == true){
-                op++
-
-            }else if (operationNum <= a) {
-                G[op].t = l
-                G.[op].bool = true;
-                operationNum++;
-
+            int operationMULT = 1;
+            int operationADD = 1;
+            int operationSUB = 1;
+            int operationDIV = 1;
+            
+            if(list[op].v == true){
+                op++;
+            }
+            else if(G[op].type == "MULT"){
+                if (operationMUX <= a)
+                    list[op].t = l;
+                    list[op].v = true;
+                    operationMULT++;
+            }
+            else if(G[op].type == "ADD"){
+                if (operationMUX <= b)
+                    list[op].t = l;
+                    list[op].v = true;
+                    operationMUX++;
+            }
+             else if(G[op].type == "SUB"){
+                if (operationMUX <= c)
+                    list[op].t = l;
+                    list[op].v = true;
+                    operationMUX++;
+            }
+            else if(G[op].type == "DIV"){
+                if (operationMUX <= d)
+                    list[op].t = l;
+                    list[op].v = true;
+                    operationMUX++;    
+            }
             }else
                 break;
             }
