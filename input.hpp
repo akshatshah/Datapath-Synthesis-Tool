d1cfa4a822e5f1cc1abe6fd9e2fa2bc7061e7f89
Akshat Shah <akshat1@mail.usf.edu>
2018-12-06 03:00:16 -0500

    Updates to input.hpp

--- input.hpp
+++ input.hpp
@@ -5,7 +5,10 @@
 
 struct operation
 {
-
+  string type;
+  string op1;
+  string op2;
+  string out;
 };
 
 struct fu_resource
@@ -32,8 +35,8 @@ struct register
     struct src_des destination[MAXNUMDESTNS];
     int sleep_ctrl_index;
     int off_steps[MAXTIME];
-    int first;//first access
-    int last;//last access
+    int t_1;//first access
+    int t_2;//last access
 };
 
 struct func_unit
@@ -59,13 +62,24 @@ struct mux
     string resource_bind;
 };
 
-vector<operation> op;
-
+struct
 int** func_compat;
 int** reg_compat;
 
+std::vector<string> input;
+std::vector<string> output;
+vector<register> registers;
+vector<operation> op;
+vector<o>;
+
+int in_bit;
+int out_bit;
+int op_bit;
+int reg_bit;
+
 void read_aif(){
   string file;
+  string line;
 
   cout<<"Enter the input file name"<<endl;
   cin>>file;
@@ -79,21 +93,73 @@ void read_aif(){
     cout<< "Input file doesn't exist";
     exit(1);
   }
-  while(line!= "outputs")
+
+  if(in_file.is_open())//if the file is open run the loops to add the functions in to the vectors
   {
+    in_file>line;
+    for(int i=0;line != "outputs";i++)
+      {
+        input.push_back(line);
+        in_file>>line;
+        if(i=0)
+        {
+          in_bit = atoi(line<<c_str);
+        }
+        in_file>>line;
+      }
 
-  }
-  while(line!="regs")
-  {
 
-  }
-  while(line!="op1")
-  {
+      in_file>>line;
+      for(int i=0;line != "regs";i++)
+        {
+          output.push_back(line);
+          in_file>>line;
+          if(i=0)
+          {
+            out_bit = atoi(line<<c_str);
+          }
+          in_file>>line;
+        }
 
-  }
+        in>>line;
+        for(i=0;line != "op1";i++)
+          {
+            registers.push_back(reg());
+            registers[i].name =line;
+            registers[i].t_1=0;
+            registers[i].t_2=0;
+            in_file>>line;
+            if(i=0)
+            {
+              reg_bit = atoi(line<<c_str);
+            }
+            in_file>>line;
+          }
 
-  while(line!="end")
-  {
+          in>>line;
+          for(i=0;line != "end";i++)
+            {
+              op.push_back(operation());
+              in_file>>line;
+              op[i].op_type = line;
+              in_file>>line;
+              if(i=0)
+              {
+                op_bit= atoi(line.c_str);
+              }
+              in_file>>line;
+              op[i].op1=line;
+              in_file>>line;
+              op[i].op2=line;
 
-  }
+              if(i=1)
+              {
+                in_bit = atoi(line<<c_str);
+              }
+              in_file>>line;
+              op[i].out=line;
+              op[i].tstep =0;
+              in_file>>line;
+            }
+}
 }
